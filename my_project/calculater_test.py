86b1ae9ed0ae1f32e86d774ca13f92b4ceed434f
lkaprielian <lisakaprielian.lk@gmail.com>
2025-06-20 17:07:28 +0200

    feat(github): add exemple calculater

---
+++ my_project/calculater_test.py
@@ -0,0 +1,26 @@
+# test_calculator.py
+
+import pytest
+from calculator import add, subtract, multiply, divide
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(-1, 1) == -2
+    assert subtract(0, 0) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 5) == 0
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(-1, 1) == -1
+    assert divide(0, 1) == 0
+    with pytest.raises(ValueError):
+        divide(1, 0)
