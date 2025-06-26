9caaaa740c4e1cd42ac2d2ad77a3e9458dd8481f
lkaprielian <lisakaprielian.lk@gmail.com>
2025-06-26 21:53:52 +0200

    feat(actions): update files

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 def addition(a, b):
     return a + b
 
-def soustraction(a, b):
-    return a - b
+
+def main(): 
+    print("Test de la fonction addition(2, 3)")
+    resultat = addition(2, 3)
+    print(f"Résultat obtenu : {resultat}")
+    assert resultat == 5
+    print("Test addition réussi\n")
+
+
+if __name__ == "__main__":
+    main()
+
+
